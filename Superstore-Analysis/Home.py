08d2005e3e2a1cd927c3b1644c968a982be7bce3
Muaaz Abu Zaid <128296319+MuaazAbuZaid@users.noreply.github.com>
2023-03-19 15:29:09 +0200

    Add files via upload

---
+++ Superstore-Analysis/Home.py
@@ -0,0 +1,18 @@
+# importing necessary libraries
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+# Page content
+st.markdown(" <center>  <h1> Superstore Analysis </h1> </font> </center> </h1> ",
+            unsafe_allow_html=True)
+
+st.markdown(''' <center>  <h6>
+    This app is created to analyze the data of a Superstore </center> </h6> ''', unsafe_allow_html=True)
+
+# the path of a photo provided from the path of this file
+st.image('Sources/Superstore.jpeg')
+
+
+
+
